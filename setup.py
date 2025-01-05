a3f7f6cf09c3d17acfac9f5e12d45fc1336256e2
Pedro Ortiz <pedrortiz89@gmail.com>
2025-01-05 16:31:11 -0600

    Inicialización del repo con formato coockiecutter ds

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Neural Network models who describe severals absortion phenomena of spectroscopy',
+    author='Pedro Ortiz',
+    license='MIT',
+)
